ee5277d37dbe344fa0054c69e91f4e3cc0208307
Khang M N <192448446+mika0798@users.noreply.github.com>
2025-10-06 11:50:47 +0700

    Create Circle.java

---
+++ Shapes/Circle.java
@@ -0,0 +1,29 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(String name, String color, double radius) {
+        super(name, color);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nShape: Circle" + "\nRadius: " + radius;
+    }
+}
