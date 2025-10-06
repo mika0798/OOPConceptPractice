e683e86a3b7c12b821f04e34d4e9c290a41b7071
Khang M N <192448446+mika0798@users.noreply.github.com>
2025-10-06 11:51:07 +0700

    Create Triangle.java

---
+++ Shapes/Triangle.java
@@ -0,0 +1,36 @@
+public class Triangle extends Shape {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(String name, String color, double side1, double side2, double side3) {
+        super(name, color);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public  double getSide1() {return side1;}
+    public  double getSide2() {return side2;}
+    public  double getSide3() {return side3;}
+    public void setSide1(double side1) {this.side1 = side1;}
+    public void setSide2(double side2) {this.side2 = side2;}
+    public void setSide3(double side3) {this.side3 = side3;}
+
+    @Override
+    public double area() {
+        double s = perimeter() / 2;
+        return Math.sqrt(s * (s-side1) * (s-side2) * (s-side3));
+    }
+
+    @Override
+    public double perimeter() {
+        return side1+side2+side3;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nShape: Triangle" + "\nSide a: " + side1 +
+                "\nSide b: " + side2 + "\nSide c: " + side3;
+    }
+}
